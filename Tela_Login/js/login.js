971ade461bae1f555498e4de8aa4b8948ad41a45
brunno51 <bruno51_@hotmail.com>
2021-11-22 16:46:38 -0300

    master

---
+++ Tela_Login/js/login.js
@@ -0,0 +1,13 @@
+function logar() {
+    var email = document.getElementById("email");
+    var senha = document.getElementById("senha");
+
+    if(email.value == "admin@admin.com" && senha.value == "admin") {
+        localStorage.setItem("acesso", true);
+        alert("Usuario autenticado");
+
+        window.location.href = "index.html"
+    }else{
+        alert("Usuario ou senha invalidos!");
+    }
+}
